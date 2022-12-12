f35ce0db6499ca4e78372e11531b1cb82c409b79
Olivier Troboe <olivier.troboe@gmail.com>
2022-12-12 14:18:51 -0500

    Update + fix ESLint/prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  importOrder: ['^[./]'],
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
--- README.md
+++ README.md
@@ -11,6 +11,15 @@ yarn start:dev
 
 # Production mode
 yarn start:prod
+
+# Check ESLint
+yarn lint:check
+
+# Check Prettier
+yarn format:check
+
+# Check all (ESLint + Prettier)
+yarn check:all
 ```
 
 ## Resources
